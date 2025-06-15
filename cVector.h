7c11d95e2c4075829f2cf8d7eef78a2ec334b48f
Vũ Xuân Thiên <24521673@gm.uit.edu.vn>
2025-06-16 01:02:38 +0700

    Create cVector.h

---
+++ cVector.h
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+class cVector {
+private:
+    int dimension;
+    float* data;
+
+public:
+    cVector(int = 0, float* = nullptr); // Vector được tạo mặc định 0 chiều, dữ liệu rỗng do chưa được xác định.
+    ~cVector();
+    cVector(const cVector&);
+    cVector& operator=(const cVector&);
+    cVector operator+(const cVector&) const;
+    cVector operator-(const cVector&) const;
+    bool operator==(const cVector&) const;
+    float dotProduct(const cVector&) const;
+    float norm() const;
+    friend std::istream& operator>>(std::istream&, cVector&);
+    friend std::ostream& operator<<(std::ostream&, const cVector&);
+    float& operator[](int);
+    float operator[](int) const;
+};
